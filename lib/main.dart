bfcf3b5a863e31023695db25fe42ae4818dfcb57
devfelpSs <devfelps@outlook.com>
2023-02-22 11:13:34 -0300

    ...

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -26,10 +28,27 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  var numeroGerado = 0;
+
+  int _gerarNumeroAleatorio(){
+    Random numeroAleatorio = Random();
+    numeroGerado = numeroAleatorio.nextInt(1000);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title:const Text("Meu App"),),
+      appBar: AppBar(
+        title: const Text("Meu App"),
+      ),
+      body: Center(child: Text(numeroGerado.toString())),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add_box), onPressed: (){
+          setState(() {
+            numeroGerado = _gerarNumeroAleatorio();
+          });
+        }),
     );
   }
-}
+} 
