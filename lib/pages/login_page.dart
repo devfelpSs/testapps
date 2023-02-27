9e14ff173232779e56a908079706746b89459916
devfelpSs <devfelps@outlook.com>
2023-02-27 11:49:30 -0300

    personalizando tela de login

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
 
 
 class LoginPage extends StatefulWidget {
@@ -12,8 +13,51 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 236, 241, 243),
+        body: SizedBox(
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 100,
+              ),
+              Container(
+                width: 150,
+                height: 150,
+                color: Colors.amber,
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+
+              Container(
+                width: double.infinity,
+                margin: const EdgeInsets.symmetric(horizontal: 30),
+                color: Colors.green,
+                height: 30,
+                alignment: Alignment.center,
+                child: const Text("Informe seu e-mail:"),
+              ),
+                            const SizedBox(
+                height: 20,
+              ),
+
+              Container(
+                width: double.infinity,
+                margin: const EdgeInsets.symmetric(horizontal: 30),
+                color: Colors.green,
+                height: 30,
+                alignment: Alignment.center,
+                child: const Text("Informe a senha:"),
+              ),
+            
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
