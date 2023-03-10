e1a58c9ab92ee5551089547ced5b8aae43d32350
devfelpSs <devfelps@outlook.com>
2023-03-10 10:05:30 -0300

    done with terms function

--- lib/shared/widgets/custom_drawer.dart
+++ lib/shared/widgets/custom_drawer.dart
@@ -61,9 +61,9 @@ class CustomDrawer extends StatelessWidget {
                   },
                 ),
                 const Divider(),
-                InkWell(
-                child: Container(
-                  padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                InkWell( //cria uma nova função chamada termos de uso e Privacidade:
+                child: Container( //cria container
+                  padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10), //adiciona um padding com 10 de borda tanto na vertical quanto na horizontal
                   width: double.infinity,
                   child: Row(
                     children: const [
@@ -72,22 +72,23 @@ class CustomDrawer extends StatelessWidget {
                       Text("Termos de uso e Privacidade"),
                     ],
                   )),
-                  onTap: (){
-                    showDialog(context: context, 
+                  onTap: (){ // quando o usuario clicar, irá executar a ação abaixo:
+                    showDialog(context: context, //mostra uma caixa de dialogo
                     builder: (BuildContext bc){
                       return AlertDialog(
                         alignment: Alignment.center,
                         elevation: 8,
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                        title: const Text("Termos de uso e Privacidade", style: TextStyle(fontWeight: FontWeight.bold),),
-                        content: Wrap(
-                          children: const [Text(" Podemos já vislumbrar o modo pelo qual a execução dos pontos do programa desafia a capacidade de equalização dos modos de operação convencionais. A nível organizacional, a expansão dos mercados mundiais estende o alcance e a importância das condições financeiras e administrativas exigidas. Todavia, o desenvolvimento contínuo de distintas formas de atuação apresenta tendências no sentido de aprovar a manutenção de alternativas às soluções ortodoxas. A prática cotidiana prova que o comprometimento entre as equipes faz parte de um processo de gerenciamento do sistema de formação de quadros que corresponde às necessidades.")],
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)), //bordas redondas no valor 10
+                        title: const Text("Termos de uso e Privacidade", style: TextStyle(fontWeight: FontWeight.bold),), //titulo do dialogo com fonte em negrito
+                        content: Wrap( //cria uma especie de Container, só que formato de acordo com o conteudo abaixo:
+                          children: const [Text(" Podemos já vislumbrar o modo pelo qual a execução dos pontos do programa desafia a capacidade de equalização dos modos de operação convencionais. A nível organizacional, a expansão dos mercados mundiais estende o alcance e a importância das condições financeiras e administrativas exigidas. Todavia, o desenvolvimento contínuo de distintas formas de atuação apresenta tendências no sentido de aprovar a manutenção de alternativas às soluções ortodoxas. A prática cotidiana prova que o comprometimento entre as equipes faz parte de um processo de gerenciamento do sistema de formação de quadros que corresponde às necessidades.",
+                          textAlign: TextAlign.center,)], //Texto com alinhamento no centro
                         ),
-                        actions: [
-                          TextButton(onPressed: (){
-                            Navigator.pop(context);
+                        actions: [ //Após o usuario ler os termos de uso, a prox ação é o usuario concordar com os termos
+                          TextButton(onPressed: (){ //cria um botão que ao ser pressionado pelo usuario
+                            Navigator.pop(context); // fecha o dialog
                           }, 
-                          child: const Text("OK"))
+                          child: const Text("OK")) //Botão com "OK", O usuario clica e concorda com os termos de uso e Privacidade
                         ],
                       );
                     }
