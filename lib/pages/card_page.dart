4fda866f78b102b90f531360eb894f694d348d5e
devfelpSs <devfelps@outlook.com>
2023-03-15 08:52:49 -0300

    Linear Progress Indicator

--- lib/pages/card_page.dart
+++ lib/pages/card_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:testapp/pages/card_detail.dart';
+import 'package:testapp/repositories/card_detail_repository.dart';
 
 import '../model/card_detail.dart';
 
@@ -13,12 +14,14 @@ class CardPage extends StatefulWidget {
  
 
 class _CardPageState extends State<CardPage> {
-  var cardDetail = CardDetail(
-    1,
-    "Meu Card",
-    "https://hermes.digitalinnovation.one/assets/diome/logo.png",
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-  );
+  CardDetail? cardDetail;
+  CardDetailRepository cardDetailRepository = CardDetailRepository();
+
+@override
+  void initState() async{
+    super.initState();
+    cardDetail = await cardDetailRepository.get();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,11 +34,11 @@ class _CardPageState extends State<CardPage> {
             onTap: (){
               Navigator.push(context, 
               MaterialPageRoute(builder: (context) => CardDetailPage(
-                cardDetail: cardDetail,
+                cardDetail: cardDetail!,
               )));
             },
             child: Hero(
-              tag: cardDetail.id,
+              tag: cardDetail!.id,
               child: Card(
                 color: Colors.grey[900], // cor de fundo do Card
                 elevation: 8, //indica como se o componente estivesse a frente, fazendo uma sombra
@@ -47,11 +50,11 @@ class _CardPageState extends State<CardPage> {
                       Row(
                         children: [
                           Image.network(
-                            cardDetail.url,  
+                            cardDetail!.url,  
                             height: 20,
                           ),
                           Text(
-                            cardDetail.title, 
+                            cardDetail!.title, 
                             style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w600, color: Colors.white),
                           ),
                         ],
