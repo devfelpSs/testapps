123829bf41a27d729679988e365187ab882705ca
devfelpSs <devfelps@outlook.com>
2023-03-16 10:50:19 -0300

    adding new listview function

--- lib/pages/list_view_h.dart
+++ lib/pages/list_view_h.dart
@@ -13,13 +13,54 @@ class _ListViewHState extends State<ListViewH> {
   Widget build(BuildContext context) {
     return ListView(
       children: [
-        ListTile(
-          leading: Image.asset(AppImages.profile2),
+       ListTile(
+         leading: Image.asset(AppImages.profile2),
           title: const Text("Perfil 2"),
-          subtitle: const Text("Oi, tudo bom ?"),
-          trailing: const Icon(Icons.menu),
+          subtitle: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              Text("Oi, tudo bom ?"),
+              Text("08:59"),
+            ],
+          ),
+          trailing: PopupMenuButton<String>(
+            onSelected: (menu){
+              if (menu == "opcao2") {} 
+            },
+            itemBuilder: (BuildContext bc){
+              return <PopupMenuEntry<String>>[
+                const PopupMenuItem(value: "opcao1", child: Text("Opção 1")),
+                const PopupMenuItem(value: "opcao2", child: Text("Opção 2")),
+                const PopupMenuItem(value: "opcao3", child: Text("Opção 3")),
+              ];
+            },
+          ), 
         ),
 
+        ListTile( //ListTile nos da uma gama muito grande de personalização, como neste caso criamos um bate papo entre 2 pessoas
+          leading: Image.asset(AppImages.profile3),
+          title: const Text("Perfil 3"),
+          subtitle: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              Text("Tudo bem e você ?"),
+              Text("09:01"),
+            ],
+          ),
+          trailing: PopupMenuButton<String>(
+            onSelected: (menu){
+              if (menu == "opcao2") {}
+            },
+            itemBuilder: (BuildContext bc){
+              return <PopupMenuEntry<String>>[
+                const PopupMenuItem(value: "opcao1", child: Text("Opção 1")),
+                const PopupMenuItem(value: "opcao2", child: Text("Opção 2")),
+                const PopupMenuItem(value: "opcao3", child: Text("Opção 3")),
+              ];
+            }),
+        ),
+
+        
         Image.asset(AppImages.profile1),
         Image.asset(AppImages.profile2),
         Image.asset(AppImages.profile3),
