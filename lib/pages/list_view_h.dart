3fc575be11d44f28a0654be0c4787d92fcf5a511
devfelpSs <devfelps@outlook.com>
2023-03-16 09:11:15 -0300

    adding listview function

---
+++ lib/pages/list_view_h.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ListViewH extends StatefulWidget {
+  const ListViewH({super.key});
+
+  @override
+  State<ListViewH> createState() => _ListViewHState();
+}
+
+class _ListViewHState extends State<ListViewH> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.yellowAccent,
+    );
+  }
+}
