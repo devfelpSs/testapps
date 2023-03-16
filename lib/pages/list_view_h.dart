a13ae2d568de9629266eaed2df3ca20ae1d96f32
devfelpSs <devfelps@outlook.com>
2023-03-16 09:45:37 -0300

    new way of view with listtile

--- lib/pages/list_view_h.dart
+++ lib/pages/list_view_h.dart
@@ -13,6 +13,13 @@ class _ListViewHState extends State<ListViewH> {
   Widget build(BuildContext context) {
     return ListView(
       children: [
+        ListTile(
+          leading: Image.asset(AppImages.profile2),
+          title: const Text("Perfil 2"),
+          subtitle: const Text("Oi, tudo bom ?"),
+          trailing: const Icon(Icons.menu),
+        ),
+
         Image.asset(AppImages.profile1),
         Image.asset(AppImages.profile2),
         Image.asset(AppImages.profile3),
