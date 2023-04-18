8a008bd738a05c4142c4663208175247ffb9b6a8
devfelpSs <devfelps@outlook.com>
2023-04-18 08:24:25 -0300

    ...

---
+++ lib/pages/numeros_aleatorios_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class NumerosAleatoriosPage extends StatefulWidget {
+  const NumerosAleatoriosPage({super.key});
+
+  @override
+  State<NumerosAleatoriosPage> createState() => _NumerosAleatoriosPageState();
+}
+
+class _NumerosAleatoriosPageState extends State<NumerosAleatoriosPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(),
+      ),
+    );
+  }
+}
