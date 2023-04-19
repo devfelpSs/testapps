2dccd9c2ee75982137fd079f9c161a52b5152fa3
devfelpSs <devfelps@outlook.com>
2023-04-19 10:07:07 -0300

    estilizando pagina de configurações

--- lib/pages/configuracoes_page.dart
+++ lib/pages/configuracoes_page.dart
@@ -8,13 +8,55 @@ class ConfiguracoesPage extends StatefulWidget {
 }
 
 class _ConfiguracoesPageState extends State<ConfiguracoesPage> {
+  TextEditingController nomeUsuarioController = TextEditingController();
+  TextEditingController alturaController = TextEditingController();
+  String? nomeUsuario;
+  double? altura;
+  bool receberNotificacoes = false;
+  bool temaEscuro = false;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(title: Text("Configurações")),
-      body: Container(),
-      ),
-    );
+        appBar: AppBar(centerTitle: true,title: const Text("Configurações")),
+      body: Container(
+        child: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: TextField(
+              keyboardType: TextInputType.name,
+              decoration: const InputDecoration(hintText: "Nome usuário"),
+              controller: nomeUsuarioController,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: TextField(
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(hintText: "Altura"),
+              controller: alturaController,
+            ),
+          ),
+        SwitchListTile(
+          title: const Text("Receber notificações"),
+          onChanged: (bool value){
+          setState(() {
+            receberNotificacoes = value;
+          });
+        }, value: receberNotificacoes,
+        ),
+        SwitchListTile(
+          title: const Text("Tema escuro"),
+          value: temaEscuro, 
+          onChanged: (bool value){
+            setState(() {
+              temaEscuro = value;
+            });
+          }),
+          TextButton(onPressed: (){}, child: Text("Salvar"))
+      ],),
+    )));
   }
 }
