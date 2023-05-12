3a1a4cfe194f401296d5278b59383dcd584e1317
devfelpSs <felipesp75@outlook.com>
2023-05-12 11:42:06 -0300

    ...

--- lib/repositories/comments/impl/comments_dio_repository.dart
+++ lib/repositories/comments/impl/comments_dio_repository.dart
@@ -1,12 +1,13 @@
 import 'package:dio/dio.dart';
 import 'package:testapp/model/comment_model.dart';
 import 'package:testapp/repositories/comments/comments_repository.dart';
+import 'package:testapp/repositories/jsonplaceholder_custom_dio.dart';
 
 class CommentsDioRepository implements CommentsRepository {
   @override
   Future<List<CommentModel>> retornaComentarios(int postId) async {
-    var dio = Dio();
-    var response = await dio.get("https://jsonplaceholder.typicode.com/posts/$postId/comments");
+    var jsonPlaceHolderCustomDio = JsonPlaceHolderCustomDio();
+    var response = await jsonPlaceHolderCustomDio.dio.get("/posts/$postId/comments");
     return (response.data as List).map((e) => CommentModel.fromJson(e)).toList();
   }
   
--- lib/repositories/posts/impl/posts_dio_repository.dart
+++ lib/repositories/posts/impl/posts_dio_repository.dart
@@ -3,13 +3,14 @@ import 'dart:convert';
 import 'package:dio/dio.dart';
 import 'package:testapp/model/post_model.dart';
 
+import '../../jsonplaceholder_custom_dio.dart';
 import 'posts_repository.dart';
 
 class PostsDioRepository implements PostsRepository{
   @override
   Future<List<PostModel>> getPosts() async {
-    var dio = Dio();
-    var response = await dio.get("https://jsonplaceholder.typicode.com/posts/");
+    var jsonPlaceHolderCustomDio = JsonPlaceHolderCustomDio();
+    var response = await jsonPlaceHolderCustomDio.dio.get("/posts/");
     if (response.statusCode == 200) {
       return (response.data as List).map((e) => PostModel.fromJson(e)).toList();
     } else {
