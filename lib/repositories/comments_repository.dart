7a1bf7b131d804aa6866d4d414729bec68f6a5ea
devfelpSs <felipesp75@outlook.com>
2023-05-12 09:44:23 -0300

    criando pagina de comentarios

---
+++ lib/repositories/comments_repository.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:testapp/model/comment_model.dart';
+
+class CommentsRepository {
+
+  Future<List<CommentModel>> retornaComentarios(int postId) async {
+    var response = await http.get(Uri.parse("https://jsonplaceholder.typicode.com/posts/$postId/comments"));
+    if (response.statusCode == 200){
+      var jsonComments = jsonDecode(response.body);
+      return (jsonComments as List)
+      .map((e) => CommentModel.fromJson(e))
+      .toList();
+    }
+    return [];
+  }
+  
+}
