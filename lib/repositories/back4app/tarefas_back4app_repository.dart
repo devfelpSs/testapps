3f37d9f23195ddaebc88618809b37bda256b9fcd
devfelpSs <felipesp75@outlook.com>
2023-05-22 10:23:33 -0300

    criando funcoes

--- lib/repositories/back4app/tarefas_back4app_repository.dart
+++ lib/repositories/back4app/tarefas_back4app_repository.dart
@@ -1,3 +1,10 @@
+import 'package:dio/dio.dart';
+import 'package:testapp/model/tarefas_back4app_model.dart';
+
 class TarefasBack4AppRepository {
-  
+  Future<TarefasBack4AppModel> obterTarefas() async {
+    var dio = Dio();
+    var result = await dio.get("https://parseapi.back4app.com/classes/Tarefas");
+    return TarefasBack4AppModel.fromJson(result.data);
+  }
 }
