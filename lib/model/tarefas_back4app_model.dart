9acfd4bbb573b45b951aed5c905157be02f481b2
devfelpSs <felipesp75@outlook.com>
2023-05-22 10:20:37 -0300

    criando novas funcoes

--- lib/model/tarefas_back4app_model.dart
+++ lib/model/tarefas_back4app_model.dart
@@ -1,41 +1,39 @@
 class TarefasBack4AppModel {
-  List<Tarefa>? results;
+  List<TarefaBack4AppModel> tarefas = [];
 
-  TarefasBack4AppModel({this.results});
+  TarefasBack4AppModel(this.tarefas);
 
   TarefasBack4AppModel.fromJson(Map<String, dynamic> json) {
     if (json['results'] != null) {
-      results = <Tarefa>[];
+      tarefas = <TarefaBack4AppModel>[];
       json['results'].forEach((v) {
-        results!.add(new Results.fromJson(v));
+        tarefas.add(TarefaBack4AppModel.fromJson(v));
       });
     }
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    if (this.results != null) {
-      data['results'] = this.results!.map((v) => v.toJson()).toList();
-    }
+    final Map<String, dynamic> data = <String, dynamic>{};
+      data['results'] = tarefas.map((v) => v.toJson()).toList();
     return data;
   }
 }
 
-class Tarefa {
-  String? objectId;
-  String? descricao;
-  bool? concluido;
-  String? createdAt;
-  String? updatedAt;
+class TarefaBack4AppModel {
+  String objectId = "";
+  String descricao = "";
+  bool concluido = false;
+  String createdAt = "";
+  String updatedAt = "";
 
-  Tarefa(
-      {this.objectId,
+  TarefaBack4AppModel(
+      this.objectId,
       this.descricao,
       this.concluido,
       this.createdAt,
-      this.updatedAt});
+      this.updatedAt);
 
-  Tarefa.fromJson(Map<String, dynamic> json) {
+  TarefaBack4AppModel.fromJson(Map<String, dynamic> json) {
     objectId = json['objectId'];
     descricao = json['descricao'];
     concluido = json['concluido'];
@@ -44,12 +42,12 @@ class Tarefa {
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['objectId'] = this.objectId;
-    data['descricao'] = this.descricao;
-    data['concluido'] = this.concluido;
-    data['createdAt'] = this.createdAt;
-    data['updatedAt'] = this.updatedAt;
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['objectId'] = objectId;
+    data['descricao'] = descricao;
+    data['concluido'] = concluido;
+    data['createdAt'] = createdAt;
+    data['updatedAt'] = updatedAt;
     return data;
   }
 }
