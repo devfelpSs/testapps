07bdc51945c2c4e5d225576ea9380def4de7818d
devfelpSs <felipesp75@outlook.com>
2023-08-21 09:06:10 -0300

    changing main class

--- lib/pages/splash_screen/homescreen.dart
+++ lib/pages/splash_screen/homescreen.dart
@@ -2,14 +2,14 @@ import "package:animated_text_kit/animated_text_kit.dart";
 import "package:flutter/material.dart";
 import "package:testapp/pages/main_page.dart";
 
-class SplashScreenDelayPage extends StatefulWidget {
-  const SplashScreenDelayPage({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
   @override
-  State<SplashScreenDelayPage> createState() => _SplashScreenDelayPageState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _SplashScreenDelayPageState extends State<SplashScreenDelayPage> {
+class _HomeScreenState extends State<HomeScreen> {
   openHome() {
     Future.delayed(const Duration(seconds: 2), () {
       Navigator.pushReplacement(
@@ -22,8 +22,14 @@ class _SplashScreenDelayPageState extends State<SplashScreenDelayPage> {
     openHome();
     return SafeArea(
       child: Scaffold(
-      body: PageView(
-
+      body: PageView(children: [
+        Container(color: Colors.yellow,),
+        Container(color: Colors.blue,),
+        Container(color: Colors.red,),
+        Container(color: Colors.green,),
+        Container(color: Colors.orange,)
+      ],
+        
     )));
   }
 }
