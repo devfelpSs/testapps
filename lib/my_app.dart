c25a17457315299f0982263e2f1017c7fd5f398c
devfelpSs <felipesp75@outlook.com>
2023-08-21 09:05:46 -0300

    Changing main class

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:testapp/pages/login_page.dart';
 import 'package:testapp/pages/splash_screen/splash_screen.dart';
-import 'package:testapp/pages/splash_screen/splashscreen_delay.dart';
+import 'package:testapp/pages/splash_screen/homescreen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreenPage(),
+      home: Home(),
     );
 
   }
